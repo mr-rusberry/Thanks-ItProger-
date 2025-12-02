3da6cf908257c01cd67cf4fc444574217309a441
Rusberry <165986852+mr-rusberry@users.noreply.github.com>
2025-12-02 18:42:11 +0500

    third update

--- exam/news/urls.py
+++ exam/news/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.news_main, name='news'),
-    path('add_news', views.add_news, name='add_news')
+    path('add_news', views.add_news, name='add_news'),
+    path('<int:pk>', views.NewsDetailView.as_view(), name='news-detail'),
 ]
--- exam/news/views.py
+++ exam/news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView
 
 
 # Create your views here.
@@ -8,13 +9,18 @@ def news_main(request):
     news = Articles.objects.order_by("-date")
     return render(request, 'news/news_main.html', {'news': news})
 
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
+
 def add_news(request):
     error = ''
     if request.method == 'POST':
         form = ArticlesForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('main')
+            return redirect('news')
         else:
             error = "Form encorrect"
 
